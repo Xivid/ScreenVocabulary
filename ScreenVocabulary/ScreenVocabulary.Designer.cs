35fa3e0c97b23c0f111cefd5d7f207d933fa2bca
Zhifei Yang <v-zhifya@microsoft.com>
2016-10-11 15:05:35 +0800

    initial commit

---
+++ ScreenVocabulary/ScreenVocabulary.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ScreenVocabulary
+{
+    partial class ScreenVocabulary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.lblMeaning = new System.Windows.Forms.Label();
+            this.btnCtrl = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblCountdown = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblWord
+            // 
+            this.lblWord.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblWord.Font = new System.Drawing.Font("Century Schoolbook", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(99, 68);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(212, 32);
+            this.lblWord.TabIndex = 0;
+            this.lblWord.Text = "Abandon";
+            this.lblWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMeaning
+            // 
+            this.lblMeaning.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblMeaning.Location = new System.Drawing.Point(41, 120);
+            this.lblMeaning.Name = "lblMeaning";
+            this.lblMeaning.Size = new System.Drawing.Size(339, 123);
+            this.lblMeaning.TabIndex = 1;
+            this.lblMeaning.Text = "放弃，放纵";
+            // 
+            // btnCtrl
+            // 
+            this.btnCtrl.Location = new System.Drawing.Point(172, 253);
+            this.btnCtrl.Name = "btnCtrl";
+            this.btnCtrl.Size = new System.Drawing.Size(75, 23);
+            this.btnCtrl.TabIndex = 2;
+            this.btnCtrl.Text = "Pause";
+            this.btnCtrl.UseVisualStyleBackColor = true;
+            this.btnCtrl.Click += new System.EventHandler(this.btnCtrl_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.Location = new System.Drawing.Point(194, 45);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(28, 13);
+            this.lblCountdown.TabIndex = 3;
+            this.lblCountdown.Text = "1:00";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(117, 12);
+            this.trackBar1.Maximum = 10000;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(185, 32);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 1000;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(304, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "10000ms";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "5ms";
+            // 
+            // ScreenVocabulary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 301);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.lblCountdown);
+            this.Controls.Add(this.btnCtrl);
+            this.Controls.Add(this.lblMeaning);
+            this.Controls.Add(this.lblWord);
+            this.Name = "ScreenVocabulary";
+            this.Text = "ScreenVocabulary";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblMeaning;
+        private System.Windows.Forms.Button btnCtrl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblCountdown;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ScreenVocabulary/ScreenVocabulary.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ScreenVocabulary
+{
+    public partial class ScreenVocabulary : Form
+    {
+        private bool isCounting = false;
+        private int timeLeft;
+        StreamReader reader;
+        List<string> words;
+        List<string> meanings;
+        private bool random = true;
+        private Random rnd;
+        int curIndex, totNum;
+
+        public ScreenVocabulary()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            this.Show();
+            reader = new StreamReader(File.OpenRead(@"3000.txt"));
+            words = new List<string>();
+            meanings = new List<string>();
+            totNum = 0;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split('\t');
+                
+                words.Add(values[0]);
+                meanings.Add(values[1]);
+
+                ++totNum;
+            }
+            rnd = new Random();
+            if(random)
+            {
+                curIndex = rnd.Next(0, totNum);
+            }
+            else
+            {
+                curIndex = 0;
+            }
+            lblWord.Text = words[curIndex];
+            lblMeaning.Text = meanings[curIndex];
+            isCounting = true;
+            timeLeft = 60;
+            timer1.Start();
+        }
+
+        private void btnCtrl_Click(object sender, EventArgs e)
+        {
+            isCounting = !isCounting;
+            btnCtrl.Text = isCounting ? "Pause" : "Resume";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isCounting == true)
+            {
+                if (timeLeft > 0)
+                {
+                    --timeLeft;
+                    lblCountdown.Text = "0:" + timeLeft.ToString("D2");
+                }
+                else
+                {
+                    timeLeft = 60;
+                    lblCountdown.Text = "1:00";
+                    if (random)
+                    {
+                        curIndex = rnd.Next(0, totNum);
+                    }
+                    else
+                    {
+                        if (++curIndex == totNum)
+                        {
+                            curIndex = 0;
+                        }
+                    }
+                    lblWord.Text = words[curIndex];
+                    lblMeaning.Text = meanings[curIndex];
+                }
+            }
+            
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+        }
+    }
+}
